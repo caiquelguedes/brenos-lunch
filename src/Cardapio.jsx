6b761a7eab448abf264626a54963a1d1c0129565
Caíque Guedes <caiqueluizghpo@gmail.com>
2024-12-11 17:45:00 -0300

    inicia a criacao do componente item

--- src/Cardapio.jsx
+++ src/Cardapio.jsx
@@ -1,11 +1,14 @@
 
 import React from 'react'
 
-export default function Cardapio(){
 
+export default function Cardapio({ nome = "Em breve...", preco = " ", decricao = " "}){
+    
     return (
         <div className="cardapio">
-            Menu
+        <div className="nome">{nome}</div>   
+        <div className="preco">{preco}</div> 
+        <div className="descricao">{descricao}</div>  
         </div>
     )
 }
