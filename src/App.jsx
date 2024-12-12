d56e81bca1cff94514ee92525e53f4d6af43b4d8
Caíque Guedes <caiqueluizghpo@gmail.com>
2024-12-12 19:55:48 -0300

    aprimora codigo e incrementa map

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import Cardapio from './Components/Cardapio/Cardapio'
 import Comidas from './Components/Comidas/Comidas'
 function App() {
   const comidas = [
-    {nome:"teste", Preco: "0,00", descricao: " "},
-    {nome:"teste2", Preco: "0,00", descricao: " "},
-    {nome:"teste3", Preco: "0,00", descricao: " "}
+    {nome:"teste", Preco: "0,00", descricao: " a"},
+    {nome:"teste2", Preco: "0,00", descricao: " b"},
+    {nome:"teste3", Preco: "0,00", descricao: " c"}
   ]
   return (
     
       <div className="app">
         <Cardapio>
-        
-          <Comidas nome = "Em breve" descricao = " "  preco = "0800"/>
-        
+        {
+        comidas.map(comida=>{
+          return (
+            <Comidas nome= {comida.nome} descricao = {comida.descricao}  preco = {comida.preco}/>
+          ) 
+        }
+      )
+        }
         </Cardapio>
         </div>
     
