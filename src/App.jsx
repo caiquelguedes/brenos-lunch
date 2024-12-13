dc5fa5974b860570dc6f5f9bc259ade24d29e1dc
Breno Machado <burracha.6464@gmail.com>
2024-12-12 21:45:38 -0300

    adiciona mais comidas

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ function App() {
   const comidas = [
     {nome:"teste", Preco: "0,00", descricao: " a"},
     {nome:"teste2", Preco: "0,00", descricao: " b"},
-    {nome:"teste3", Preco: "0,00", descricao: " c"}
+    {nome:"teste3", Preco: "0,00", descricao: " c"},
+    {nome:"teste2", Preco: "0,00", descricao: " b"},
+    {nome:"teste2", Preco: "0,00", descricao: " b"},
+    {nome:"teste2", Preco: "0,00", descricao: " b"},
+    {nome:"teste2", Preco: "0,00", descricao: " b"},
+    {nome:"teste2", Preco: "0,00", descricao: " b"},
+    {nome:"teste2", Preco: "0,00", descricao: " b"},
+    {nome:"teste2", Preco: "0,00", descricao: " b"}
   ]
   return (
     
